96879f594882b5e80d1dd30e73682523e676bea1
JH <kojh0111@naver.com>
2021-07-14 13:58:41 +0900

    Data Manipulation Language in python

--- main.py
+++ main.py
@@ -9,7 +9,21 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-user = UserModel("username", "1234", "JH", "me@jh.ko")
+# Insert user
+user = UserModel("username", "1234", "JH", "jh@bo.b")
 session.add(user)
+
+# select user by name
+selected_users = session.query(UserModel).filter(UserModel.name == "bob").all()
+bob = session.query(UserModel).filter(UserModel.name == "bob").one_or_none()
+
+# Update password
+bob.password = "2345"
+session.add(bob)
+session.commit()
+
+# delete user by name
+jh = session.query(UserModel).filter(UserModel.name == "JH").one_or_none()
+session.delete(jh)
 
 session.commit()
